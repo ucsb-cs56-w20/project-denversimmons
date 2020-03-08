29fa91293185c22fbb1ad0c0459b762798cc16d2
Denver Simmons <denversimmons@umail.ucsb.edu>
2020-03-07 16:03:48 -0800

    ds - LocationQueryService return just url

--- src/main/java/edu/ucsb/cs56/w20/lab07/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/services/LocationQueryService.java
@@ -53,7 +53,7 @@ public class LocationQueryService {
             retVal = "{\"error\": \"401: Unauthorized\"}";
         }
         logger.info("from LocationQueryService.getJSON: " + retVal);
-        return url;
+        return retVal;
     }
 
 } 
