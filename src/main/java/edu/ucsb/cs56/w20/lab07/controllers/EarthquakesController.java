fedd5c18722b7d8da7cd6e74ff755c03c7052e60
Denver Simmons <denversimmons@umail.ucsb.edu>
2020-03-07 22:05:45 -0800

    ds - Added true Lat and Lon to EarthquakeQueryService

--- src/main/java/edu/ucsb/cs56/w20/lab07/controllers/EarthquakesController.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/EarthquakesController.java
@@ -26,7 +26,7 @@ public class EarthquakesController {
 	 EarthquakeQueryService e = new EarthquakeQueryService();
 	 model.addAttribute("eqSearch", eqSearch);
         // TODO: Actually do the search here and add results to the model
-	 String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+	 String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag(), eqSearch.getLat(), eqSearch.getLon());
          model.addAttribute("json", json);
 	 FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
          model.addAttribute("featureCollection",featureCollection);
