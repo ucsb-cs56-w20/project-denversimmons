60305d7dfc19493a6e9269c1a5ccdb0771405f48
Denver Simmons <denversimmons@umail.ucsb.edu>
2020-03-10 22:19:46 -0700

    ds - recopied delete code from instructions. probably wont do anything

--- src/main/java/edu/ucsb/cs56/w20/lab07/controllers/LocationsController.java
+++ src/main/java/edu/ucsb/cs56/w20/lab07/controllers/LocationsController.java
@@ -42,11 +42,11 @@ public class LocationsController {
 
     @DeleteMapping("/locations/delete/{id}")
     public String delete(@PathVariable("id") long id, Model model) {
-	Location location = locationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
+    Location location = locationRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid courseoffering Id:" + id));
     locationRepository.delete(location);
     model.addAttribute("locations", locationRepository.findAll());
-    return "locations/index"; 
-    }
+    return "locations/index";
+}
     
     @GetMapping("/locations")
     public String locations(Model model, OAuth2AuthenticationToken token,
